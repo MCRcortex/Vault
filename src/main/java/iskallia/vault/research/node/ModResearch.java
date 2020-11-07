1143a977fd3f5b0c408634546fbe9ee0ca5d3977
Anılcan <igoodie@programmer.net>
2020-11-07 03:25:42 +0300

    feat(config): default iskall's new Research configs

--- src/main/java/iskallia/vault/research/node/ModResearch.java
+++ src/main/java/iskallia/vault/research/node/ModResearch.java
@@ -32,6 +32,19 @@ public class ModResearch extends Research {
         return restrictions;
     }
 
+    public ModResearch withRestrictions(boolean hittability,
+                                        boolean entityIntr,
+                                        boolean blockIntr,
+                                        boolean usability,
+                                        boolean craftability) {
+        this.restrictions.set(Restrictions.Type.HITTABILITY, hittability);
+        this.restrictions.set(Restrictions.Type.ENTITY_INTERACTABILITY, entityIntr);
+        this.restrictions.set(Restrictions.Type.BLOCK_INTERACTABILITY, blockIntr);
+        this.restrictions.set(Restrictions.Type.USABILITY, usability);
+        this.restrictions.set(Restrictions.Type.CRAFTABILITY, craftability);
+        return this;
+    }
+
     @Override
     public boolean restricts(Item item, Restrictions.Type restrictionType) {
         if (!this.restrictions.restricts(restrictionType)) return false;
