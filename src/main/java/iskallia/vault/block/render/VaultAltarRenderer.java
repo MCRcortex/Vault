e8d3785067ff951ac3ec0511feb7d6a9b9c5cd41
jmilthedude <jmiltheman@gmail.com>
2020-11-13 02:49:34 -0600

    feat(altar): almost done with rendering

--- src/main/java/iskallia/vault/block/render/VaultAltarRenderer.java
+++ src/main/java/iskallia/vault/block/render/VaultAltarRenderer.java
@@ -1,10 +1,6 @@
 package iskallia.vault.block.render;
 
-import java.util.Map;
-import java.util.UUID;
-
 import com.mojang.blaze3d.matrix.MatrixStack;
-
 import iskallia.vault.altar.PedestalItem;
 import iskallia.vault.block.entity.VaultAltarTileEntity;
 import iskallia.vault.init.ModBlocks;
@@ -21,96 +17,109 @@ import net.minecraft.client.renderer.tileentity.TileEntityRenderer;
 import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.vector.Matrix4f;
 import net.minecraft.util.math.vector.Vector3f;
+import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.world.LightType;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 
+import java.util.Map;
+import java.util.UUID;
+
 public class VaultAltarRenderer extends TileEntityRenderer<VaultAltarTileEntity> {
 
-	private float currentTick = 0;
+    private float currentTick = 0;
 
-	public VaultAltarRenderer(TileEntityRendererDispatcher rendererDispatcherIn) {
-		super(rendererDispatcherIn);
-	}
+    public VaultAltarRenderer(TileEntityRendererDispatcher rendererDispatcherIn) {
+        super(rendererDispatcherIn);
+    }
 
-	@Override
-	public void render(VaultAltarTileEntity altar, float partialTicks, MatrixStack matrixStack, IRenderTypeBuffer buffer, int combinedLight, int combinedOverlay) {
-		if (!altar.containsVaultRock())
-			return;
+    public static void register() {
+        ClientRegistry.bindTileEntityRenderer(ModBlocks.VAULT_ALTAR_TILE_ENTITY, VaultAltarRenderer::new);
+    }
 
-		Minecraft mc = Minecraft.getInstance();
-		ClientPlayerEntity player = mc.player;
+    @Override
+    public void render(VaultAltarTileEntity altar, float partialTicks, MatrixStack matrixStack, IRenderTypeBuffer buffer, int combinedLight, int combinedOverlay) {
+        if (!altar.containsVaultRock())
+            return;
 
-		Map<UUID, PedestalItem[]> nearbyPlayers = altar.getNearbyPlayers();
-		if (!nearbyPlayers.containsKey(player.getUniqueID()))
-			return;
+        Minecraft mc = Minecraft.getInstance();
+        ClientPlayerEntity player = mc.player;
 
-		PedestalItem[] items = nearbyPlayers.get(player.getUniqueID());
+        Map<UUID, PedestalItem[]> nearbyPlayers = altar.getNearbyPlayers();
+        if (!nearbyPlayers.containsKey(player.getUniqueID()))
+            return;
 
-		ItemRenderer itemRenderer = mc.getItemRenderer();
-		FontRenderer fontRenderer = mc.fontRenderer;
-		float angle = getAngle(player, partialTicks);
-		int lightLevel = getLightAtPos(altar.getWorld(), altar.getPos().up());
+        PedestalItem[] items = nearbyPlayers.get(player.getUniqueID());
 
-		for (int i = 0; i < items.length; i++) {
+        ItemRenderer itemRenderer = mc.getItemRenderer();
+        FontRenderer fontRenderer = mc.fontRenderer;
+        //float angle = getAngle(player, partialTicks);
+        int lightLevel = getLightAtPos(altar.getWorld(), altar.getPos().up());
 
-			matrixStack.push();
+        for (int i = 0; i < items.length; i++) {
 
-			double[] corner = getCorner(i);
-			matrixStack.translate(corner[0], corner[1], corner[2]);
-			matrixStack.rotate(Vector3f.YP.rotationDegrees(angle * 10f));
+            //render item at index
+            matrixStack.push();
+            double[] corner = getCorner(i);
+            matrixStack.translate(corner[0], corner[1], corner[2]);
+            matrixStack.rotate(Vector3f.YP.rotationDegrees(180.0F - player.rotationYaw));
+            IBakedModel ibakedmodel = itemRenderer.getItemModelWithOverrides(items[i].getItem(), altar.getWorld(), null);
+            itemRenderer.renderItem(items[i].getItem(), TransformType.GROUND, true, matrixStack, buffer, lightLevel, combinedOverlay, ibakedmodel);
+            matrixStack.pop();
 
-			IBakedModel ibakedmodel = itemRenderer.getItemModelWithOverrides(items[i].getItem(), altar.getWorld(), null);
-			itemRenderer.renderItem(items[i].getItem(), TransformType.GROUND, true, matrixStack, buffer, lightLevel, combinedOverlay, ibakedmodel);
+            //render amount required for the item
+            matrixStack.push();
+            StringTextComponent text = new StringTextComponent(String.valueOf(items[i].getAmountRequired()));
+            float scale = 0.01f;
+            int opacity = (int) (.4f * 255.0F) << 24;
+            float offset = (float) (-fontRenderer.getStringPropertyWidth(text) / 2);
+            Matrix4f matrix4f = matrixStack.getLast().getMatrix();
 
-			matrixStack.pop();
+            matrixStack.translate(corner[0], corner[1] + .4f, corner[2]);
+            matrixStack.scale(scale, scale, scale);
+            matrixStack.rotate(mc.getRenderManager().getCameraOrientation());
+            matrixStack.rotate(Vector3f.ZP.rotationDegrees(180.0F));
+            fontRenderer.func_243247_a(text, offset, 0, 0xffffff, false, matrix4f, buffer, false, opacity, lightLevel);
+            matrixStack.pop();
 
-			matrixStack.push();
-			fontRenderer.drawString(matrixStack, "1234", 5, 5, 5);
-			matrixStack.pop();
+        }
 
-		}
+        //render vault rock
+        matrixStack.push();
+        matrixStack.translate(.5f, 1.75f, .5f);
+        matrixStack.rotate(Vector3f.YP.rotationDegrees(180.0F - player.rotationYaw));
+        ItemStack vaultRock = new ItemStack(ModItems.VAULT_ROCK);
+        IBakedModel ibakedmodel = itemRenderer.getItemModelWithOverrides(vaultRock, altar.getWorld(), null);
+        itemRenderer.renderItem(vaultRock, TransformType.GROUND, true, matrixStack, buffer, lightLevel, combinedOverlay, ibakedmodel);
+        matrixStack.pop();
 
-		matrixStack.push();
+    }
 
-		matrixStack.translate(.5f, 1.1f, .5f);
+    private float getAngle(ClientPlayerEntity player, float partialTicks) {
+        currentTick = player.ticksExisted;
+        float angle = (currentTick + partialTicks) % 360;
+        return angle;
+    }
 
-		ItemStack vaultRock = new ItemStack(ModItems.VAULT_ROCK);
-		IBakedModel ibakedmodel = itemRenderer.getItemModelWithOverrides(vaultRock, altar.getWorld(), null);
-		itemRenderer.renderItem(vaultRock, TransformType.GROUND, true, matrixStack, buffer, lightLevel, combinedOverlay, ibakedmodel);
+    private int getLightAtPos(World world, BlockPos pos) {
+        int blockLight = world.getLightFor(LightType.BLOCK, pos);
+        int skyLight = world.getLightFor(LightType.SKY, pos);
+        return LightTexture.packLight(blockLight, skyLight);
+    }
 
-		matrixStack.pop();
-
-	}
-
-	private float getAngle(ClientPlayerEntity player, float partialTicks) {
-		currentTick = player.ticksExisted;
-		float angle = (currentTick + partialTicks) % 360;
-		return angle;
-	}
-
-	private int getLightAtPos(World world, BlockPos pos) {
-		int blockLight = world.getLightFor(LightType.BLOCK, pos);
-		int skyLight = world.getLightFor(LightType.SKY, pos);
-		return LightTexture.packLight(blockLight, skyLight);
-	}
-
-	private double[] getCorner(int index) {
-		switch (index) {
-		case 0:
-			return new double[] { .9, 1.5, 0.1 };
-		case 1:
-			return new double[] { .9, 1.5, .9 };
-		case 2:
-			return new double[] { 0.1, 1.5, .9 };
-		default:
-			return new double[] { 0.1, 1.5, 0.1 };
-		}
-	}
-
-	public static void register() {
-		ClientRegistry.bindTileEntityRenderer(ModBlocks.VAULT_ALTAR_TILE_ENTITY, VaultAltarRenderer::new);
-	}
+    private double[] getCorner(int index) {
+        switch (index) {
+            case 0:
+                return new double[]{.9, 1.1, 0.1};
+            case 1:
+                return new double[]{.9, 1.1, .9};
+            case 2:
+                return new double[]{0.1, 1.1, .9};
+            default:
+                return new double[]{0.1, 1.1, 0.1};
+        }
+    }
 
 }
--- src/main/java/iskallia/vault/config/VaultPedestalConfig.java
+++ src/main/java/iskallia/vault/config/VaultPedestalConfig.java
@@ -14,61 +14,61 @@ import net.minecraftforge.registries.ForgeRegistries;
 
 public class VaultPedestalConfig extends Config {
 
-	@Expose
-	public List<PedestalConfigItem> ITEMS = new ArrayList<PedestalConfigItem>();
+    @Expose
+    public List<PedestalConfigItem> ITEMS = new ArrayList<PedestalConfigItem>();
 
-	private Random rand = new Random();
+    private Random rand = new Random();
 
-	@Override
-	public String getName() {
-		return "pedestal_items";
-	}
+    @Override
+    public String getName() {
+        return "pedestal_items";
+    }
 
-	@Override
-	protected void reset() {
+    @Override
+    protected void reset() {
 
-		ITEMS.add(new PedestalConfigItem("minecraft:stone", 1000, 32000));
-		ITEMS.add(new PedestalConfigItem("minecraft:cobblestone", 1000, 32000));
-		ITEMS.add(new PedestalConfigItem("minecraft:diamond", 1000, 32000));
-		ITEMS.add(new PedestalConfigItem("minecraft:gold_nugget", 1000, 32000));
+        ITEMS.add(new PedestalConfigItem("minecraft:stone", 1000, 32000));
+        ITEMS.add(new PedestalConfigItem("minecraft:cobblestone", 1000, 32000));
+        ITEMS.add(new PedestalConfigItem("minecraft:diamond", 1000, 32000));
+        ITEMS.add(new PedestalConfigItem("minecraft:gold_nugget", 1000, 32000));
 
-	}
+    }
 
-	public PedestalItem[] getRequiredItemsFromConfig() {
-		PedestalItem[] pedestalItems = new PedestalItem[4];
+    public PedestalItem[] getRequiredItemsFromConfig() {
+        PedestalItem[] pedestalItems = new PedestalItem[4];
 
-		List<PedestalConfigItem> configItems = new ArrayList<PedestalConfigItem>();
-		configItems.addAll(ITEMS);
+        List<PedestalConfigItem> configItems = new ArrayList<PedestalConfigItem>();
+        configItems.addAll(ITEMS);
 
-		for (int i = 0; i < pedestalItems.length; i++) {
-			PedestalConfigItem configItem = configItems.remove(rand.nextInt(configItems.size()));
-			Item item = ForgeRegistries.ITEMS.getValue(new ResourceLocation(configItem.ITEM_ID));
+        for (int i = 0; i < pedestalItems.length; i++) {
+            PedestalConfigItem configItem = configItems.remove(rand.nextInt(configItems.size()));
+            Item item = ForgeRegistries.ITEMS.getValue(new ResourceLocation(configItem.ITEM_ID));
 
-			pedestalItems[i] = new PedestalItem(new ItemStack(item), getRandomInt(configItem.MIN, configItem.MAX), 0);
-		}
+            pedestalItems[i] = new PedestalItem(new ItemStack(item), 0, getRandomInt(configItem.MIN, configItem.MAX));
+        }
 
-		return pedestalItems;
-	}
+        return pedestalItems;
+    }
 
-	private int getRandomInt(int min, int max) {
-		return (int) (Math.random() * (max - min) + min);
-	}
+    private int getRandomInt(int min, int max) {
+        return (int) (Math.random() * (max - min) + min);
+    }
 
-	public class PedestalConfigItem {
+    public class PedestalConfigItem {
 
-		@Expose
-		public String ITEM_ID;
-		@Expose
-		public int MIN;
-		@Expose
-		public int MAX;
+        @Expose
+        public String ITEM_ID;
+        @Expose
+        public int MIN;
+        @Expose
+        public int MAX;
 
-		public PedestalConfigItem(String item, int min, int max) {
-			ITEM_ID = item;
-			MIN = min;
-			MAX = max;
-		}
+        public PedestalConfigItem(String item, int min, int max) {
+            ITEM_ID = item;
+            MIN = min;
+            MAX = max;
+        }
 
-	}
+    }
 
 }
