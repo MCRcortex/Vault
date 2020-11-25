4c673d69455b519e685cc9e1762a09d5ea57b79f
jmilthedude <30945719+jmilthedude@users.noreply.github.com>
2020-11-24 22:27:19 -0600

    feat(crate): added crate block class

---
+++ src/main/java/iskallia/vault/block/VaultCrateBlock.java
@@ -0,0 +1,9 @@
+package iskallia.vault.block;
+
+import net.minecraft.block.Block;
+
+public class VaultCrateBlock extends Block {
+    public VaultCrateBlock(Properties properties) {
+        super(properties);
+    }
+}
