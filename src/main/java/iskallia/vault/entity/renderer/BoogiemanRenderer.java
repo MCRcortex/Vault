d84d7c5ae39c9cbaad9992d1119076253cc73a0c
Anılcan <igoodie@programmer.net>
2020-12-10 20:00:14 +0300

    fix(render): boogieman armor isn't hidden

--- src/main/java/iskallia/vault/entity/renderer/BoogiemanRenderer.java
+++ src/main/java/iskallia/vault/entity/renderer/BoogiemanRenderer.java
@@ -2,8 +2,11 @@ package iskallia.vault.entity.renderer;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
 import iskallia.vault.Vault;
+import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.client.renderer.entity.ZombieRenderer;
+import net.minecraft.client.renderer.entity.layers.LayerRenderer;
+import net.minecraft.client.renderer.entity.model.ZombieModel;
 import net.minecraft.entity.monster.ZombieEntity;
 import net.minecraft.util.ResourceLocation;
 
@@ -13,6 +16,13 @@ public class BoogiemanRenderer extends ZombieRenderer {
 
     public BoogiemanRenderer(EntityRendererManager renderManagerIn) {
         super(renderManagerIn);
+
+//        for (int i = layerRenderers.size() - 1; i >= 0; i--) {
+//            LayerRenderer<ZombieEntity, ZombieModel<ZombieEntity>> layer = layerRenderers.get(i);
+//            System.out.println(i + " " + layer.getClass().getSimpleName());
+//        }
+
+        layerRenderers.remove(this.layerRenderers.size() - 1); // Do not render armors
     }
 
     @Override
